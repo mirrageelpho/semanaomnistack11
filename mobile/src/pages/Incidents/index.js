11520dca604fb21573769b4f0afab322a7aefb40
Aecio Cleber Rodrigues Pires <carp@iMac.local>
2020-03-27 21:46:13 -0300

    Create Mobile Interface

---
+++ mobile/src/pages/Incidents/index.js
@@ -0,0 +1,48 @@
+import * as  React from 'react';
+import { Text, View, Image, TouchableOpacity, FlatList } from 'react-native';
+import { Feather } from '@expo/vector-icons';
+import logoImg from '../../assets/logo.png'
+
+import styles from './styles';
+
+export default function Incidents() {
+    return (
+        <View style={styles.container}>
+            <View style={styles.header}>
+                <Image source={logoImg} />
+                <Text style={styles.headerText}>
+                    Total de <Text style={styles.headerTextBold}> 0 casos </Text>
+                </Text>
+            </View>
+            <Text style={styles.title}>Bem-vindo!</Text>
+            <Text style={styles.description}>Escolha um dos casos abaixo e sale-o</Text>
+
+            <FlatList
+                style={styles.incidentsList}
+                data={[1, 2, 3]}
+                keyExtractor={incident => String(incident)}
+                showsVerticalScrollIndicator={false}
+                renderItem={() => (
+                    <View style={styles.incident}>
+                        <Text style={styles.incidentProperty}>ONG:</Text>
+                        <Text style={styles.incidentValue}>APAD</Text>
+
+                        <Text style={styles.incidentProperty}>CASO:</Text>
+                        <Text style={styles.incidentValue}>Cadelinha dodoi =(</Text>
+
+                        <Text style={styles.incidentProperty}>VALUE:</Text>
+                        <Text style={styles.incidentValue}>R$ 120,00</Text>
+
+                        <TouchableOpacity
+                            style={styles.detailsButton}
+                            onPress={() => { }}
+                        >
+                            <Text style={styles.detailsButtonText}>Ver mais detalhes</Text>
+                            <Feather name="arrow-right" size={16} color="#E02041" />
+                        </TouchableOpacity>
+                    </View>
+                )}
+            />
+        </View>
+    )
+}
